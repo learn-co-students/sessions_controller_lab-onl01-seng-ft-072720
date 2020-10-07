c7c984c3e0524dae729725a88193d0f4dee87943
Zach_Kobrinsky <zachkobrinsky@gmail.com>
2020-10-07 15:20:25 -0400

    Done.

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,14 @@
 module SessionsHelper
+
+    def logged_in?
+        !!session[:name]
+    end 
+    def display_greeting
+        if logged_in?
+          "Hi, #{session[:name]}"
+        else
+          "Hi."
+        end
+    end
+
 end
